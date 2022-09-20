fbdb8096bfe3076cc42fc7097c344b5280cf9308
Michael Haberler <git@mah.priv.at>
2022-09-20 09:44:15 +0200

    add POST handling, optional params

--- airspaces.py
+++ airspaces.py
@@ -4,8 +4,6 @@ import sqlite3
 import uuid
 
 # function that makes query results return lists of dictionaries instead of lists of tuples
-
-
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
@@ -21,9 +19,9 @@ def read_file(name):
 def read_json_file(name, asGeojson=False, object_hook=None):
     s = read_file(name).decode()
     if asGeojson:
-        return geojson.loads(s,  object_hook=object_hook)
+        return geojson.loads(s, object_hook=object_hook)
     else:
-        return json.loads(s,  object_hook=object_hook)
+        return json.loads(s, object_hook=object_hook)
 
 
 def get_all_keys(d):
@@ -38,7 +36,7 @@ def fixup(row, schema):
         if k in schema:
             d[k] = schema[k][d[k]]
     return row
-    
+
 
 class Airspaces:
     def __init__(self, mbtiles="", schema=None, spatialite=None):
@@ -49,10 +47,14 @@ class Airspaces:
         # apply the function to the sqlite3 engine
         self.conn.row_factory = dict_factory
         self.cursor = self.conn.cursor()
-        self.schema = read_json_file(schema,
-                                     object_hook=lambda d: {int(k) if k.isnumeric() else k: v for k, v in d.items()})
+        self.schema = read_json_file(
+            schema,
+            object_hook=lambda d: {
+                int(k) if k.isnumeric() else k: v for k, v in d.items()
+            },
+        )
 
-    def lookup(self, lat, lon, alt=None):
+    def lookup(self, lat, lon, alt=None, shapes=False):
         query = (
             f"select properties, AsGeoJSON(geometry) from features "
             f"where MBRWithin(ST_Point({lon},{lat}), geometry) and  "
@@ -62,52 +64,55 @@ class Airspaces:
         data = self.cursor.fetchall()
         queryprops = {
             "name": "query",
-            "within" : [],
-            "above" : [],
-            "below" : [],
+            "within": [],
+            "above": [],
+            "below": [],
         }
         # create a new list which will store the single GeoJSON features
         featureCollection = list()
- 
+
         for row in data:
             # create a single GeoJSON geometry from the geometry column which already contains a GeoJSON string
-            geom = geojson.loads(row['AsGeoJSON(geometry)'])
+            geom = geojson.loads(row["AsGeoJSON(geometry)"])
 
             # remove the geometry field from the current's row's dictionary
-            row.pop('AsGeoJSON(geometry)')
+            row.pop("AsGeoJSON(geometry)")
 
             # create a new GeoJSON feature and pass the geometry columns as well as all remaining attributes which are stored in the row dictionary
-            j = json.loads(row['properties'])
+            j = json.loads(row["properties"])
             props = fixup(j, self.schema)
             if alt:
-                (above_lower_boundary, below_upper_boundary) = self.test_current_airspace(props, alt)
-                if (above_lower_boundary and below_upper_boundary):
-                    queryprops['within'].append(props)
+                (
+                    above_lower_boundary,
+                    below_upper_boundary,
+                ) = self.test_current_airspace(props, alt)
+                if above_lower_boundary and below_upper_boundary:
+                    queryprops["within"].append(props)
                 elif above_lower_boundary:
-                    queryprops['above'].append(props)
-                elif below_upper_boundary: 
-                    queryprops['below'].append(props)
+                    queryprops["above"].append(props)
+                elif below_upper_boundary:
+                    queryprops["below"].append(props)
 
-                #print("lookup", alt, above_lower_boundary, below_upper_boundary, props["lowerLimit"], props["upperLimit"])
+                # print("lookup", alt, above_lower_boundary, below_upper_boundary, props["lowerLimit"], props["upperLimit"])
+            if shapes:
+                feature = geojson.Feature(geometry=geom, properties=props, id=props["_id"])
 
-            feature = geojson.Feature(geometry=geom, properties=props, id=props["_id"])
-
-            # append the current feature to the list of all features
-            featureCollection.append(feature)
+                # append the current feature to the list of all features
+                featureCollection.append(feature)
 
         if alt:
             # add the query as point feature
             loc = geojson.Feature(
-                    geometry=geojson.Point((lon, lat, alt)),
-                    properties=queryprops,
-                    id=uuid.uuid4()
-                )
+                geometry=geojson.Point((lon, lat, alt)),
+                properties=queryprops,
+                id=uuid.uuid4(),
+            )
             featureCollection.append(loc)
 
-        return {"type": "FeatureCollection",  "features": featureCollection}
+        return {"type": "FeatureCollection", "features": featureCollection}
 
-    def within_airspace(self, altitude_ft, upper, upper_unit,  lower, lower_unit):
-        fl = int(altitude_ft/100)
+    def within_airspace(self, altitude_ft, upper, upper_unit, lower, lower_unit):
+        fl = int(altitude_ft / 100)
         below_upper_boundary = False
         above_lower_boundary = False
         if upper_unit == "Feet":
@@ -130,5 +135,5 @@ class Airspaces:
             lower_unit = feature["lowerLimit"]["unit"]
             # lower_ref = feature["lowerLimit"]["referenceDatum"]
         except Exception:
-                return (False, False)
+            return (False, False)
         return self.within_airspace(altitude_ft, upper, upper_unit, lower, lower_unit)
--- app.py
+++ app.py
@@ -1,27 +1,40 @@
-
-
 # uvicorn app:app --reload --port 1815
 
 # curl -X 'GET'   http://127.0.0.1:1815/
 # curl -X 'GET'   http://127.0.0.1:1815/v1/airspaces/47.1292371/15.2120164/7100
 # curl -X 'GET'   http://127.0.0.1:1815/v1/airspaces/47.1292371/15.2120164
 
+from typing import Union
 import uvicorn
 import logging
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 from airspaces import Airspaces
 
 MBTILES = "./airspaces.db"
 SCHEMA = "./schema.json"
-SPATIALITE = '/usr/lib/x86_64-linux-gnu/mod_spatialite.so'
+SPATIALITE = "/usr/lib/x86_64-linux-gnu/mod_spatialite.so"
 
 app = FastAPI()
 airspaces = Airspaces(mbtiles=MBTILES, schema=SCHEMA, spatialite=SPATIALITE)
 
+
+class Location(BaseModel):
+    lat: float
+    lon: float
+    alt: Union[float, None] = None
+    shapes: Union[bool, None] = None
+
+
 @app.get("/")
 async def root():
-    logging.warn("ROOT")
     return {"message": "Hello World"}
+
+
+@app.post("/v1/airspaces/")
+async def lookup_location(item: Location):
+    return airspaces.lookup(item.lat, item.lon, alt=item.alt, shapes=item.shapes)
 
 
 @app.get("/v1/airspaces/{lat}/{lon}/{alt}")
@@ -34,6 +47,7 @@ async def read_pos(lat: float, lon: float, alt: float):
 @app.get("/v1/airspaces/{lat}/{lon}/", include_in_schema=False)
 async def read_item(lat: float, lon: float):
     return airspaces.lookup(lat, lon)
+
 
 if __name__ == "__main__":
     config = uvicorn.run("app", port=1815, log_level="debug")
